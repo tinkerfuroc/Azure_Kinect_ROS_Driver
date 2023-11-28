520c7bc5e6d69772fbcca54f1bd4e6807459cf2c
queenxy <1060382399@qq.com>
2023-11-28 23:15:04 +0800

    fps and resolution change

--- launch/driver.launch.py
+++ launch/driver.launch.py
@@ -75,11 +75,11 @@ def generate_launch_description():
         description="The format of RGB camera. Valid options: bgra, jpeg"),
     DeclareLaunchArgument(
         'color_resolution',
-        default_value="1536P",
+        default_value="720P",
         description="Resolution at which to run the color camera. Valid options: 720P, 1080P, 1440P, 1536P, 2160P, 3072P"),
     DeclareLaunchArgument(
         'fps',
-        default_value="5",
+        default_value="15",
         description="FPS to run both cameras at. Valid options are 5, 15, and 30"),
     DeclareLaunchArgument(
         'point_cloud',
